61e7330586e182dfb9eeb5936ec595f88d07f323
myluckyhub1994 <luckydx1994@gmail.com>
2024-01-04 17:26:55 +0530

    express

--- express/index.js
+++ express/index.js
@@ -2,15 +2,30 @@ const express = require('express');
 const app = express();
 
 app.get("", (req, resp) => { 
-    resp.send("Welcome, this is Homepage");
+
+    resp.send(`<h1>Welcome, this is Homepage</h1>
+    <a href="/about">Go to About page</a>`);
 });
 
 app.get("/about", (req, resp) => {
-    resp.send("Welcome, This is a About Page");
+    resp.send(`
+   <input type="text" placeholder="User Name" />
+   <button>Click Me</button>
+   <a href="/">Go to Homepage</a>
+   `);
 });
 
 app.get("/help", (req, resp) => {
-    resp.send("Welcome, This is a Help Page");
+    resp.send([
+        {
+            name: 'puneet',
+            email: 'luckydx4@gmail.com'
+        },
+        {
+            name: 'Samitht',
+            email: 'lucdcdx4@gmail.com'
+        }
+    ]);
 });
 
 
