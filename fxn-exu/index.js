c583792ad8e6e4bf407602eb23993247f466273e
myluckyhub1994 <luckydx1994@gmail.com>
2024-01-04 15:50:21 +0530

    fnx-exu

---
+++ fxn-exu/index.js
@@ -0,0 +1,11 @@
+console.log("starting up")
+
+setTimeout(()=>{
+console.log("2 second wait")
+},2000);
+
+setTimeout(()=>{
+    console.log("0 second wait")
+    },0);
+
+console.log("finishing up")
